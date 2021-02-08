de04f0ceb0577f7c0d28c643ec17c8e477454e6e
Eco <eco@pop-os.localdomain>
2021-02-08 13:18:54 +0800

    Changing to Typescript

--- back_end/index.js
+++ back_end/index.js
@@ -1,8 +1,28 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+import userRoutes from './routes/UserRoutes.js';
 
+dotenv.config()
 const app = express();
 const PORT = process.env.PORT  || 3000;
 
+
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+// User
+app.use('/',userRoutes);
+
+mongoose.connect(process.env.DATABASE_URL,{
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+},()=>{
+    console.log("Database connected");
+})
+
+
 app.listen(PORT, ()=>{
-    console.log("TUP Server is Running!");
+    console.log(`TUP Server is Running at ${PORT}`);
 })
